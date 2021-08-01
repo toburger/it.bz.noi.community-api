146bd682019d95b1252dda2e15f5697d225336de
Alex Lanz <alex.lanz@aboutbits.it>
2021-08-01 10:34:55 +0200

    CI pipeline

---
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'infrastructure/docker/dotnet/Dockerfile'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    stages {
+        stage('Restore Dependencies') {
+            steps {
+                sh 'dotnet restore'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'dotnet build --no-restore'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'dotnet test --no-build --verbosity normal'
+            }
+        }
+    }
+}
