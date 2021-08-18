d0e96bd855b65723322a697b83608cd25829839e
Alex Lanz <alex.lanz@aboutbits.it>
2021-08-18 15:48:24 +0200

    CI optimizations

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -4,18 +4,18 @@ pipeline {
     stages {
         stage('Restore Dependencies') {
             steps {
-                sh 'docker run --rm -v ${PWD}:/code -w /code mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet tool restore'
-                sh 'docker run --rm -v ${PWD}:/code -w /code mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet restore'
+                sh 'docker run --rm -v ${PWD}:/code -w /code -v ${PWD}/infrastructure/docker/dotnet/.nuget:/root/.nuget mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet tool restore'
+                sh 'docker run --rm -v ${PWD}:/code -w /code -v ${PWD}/infrastructure/docker/dotnet/.nuget:/root/.nuget mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet restore'
             }
         }
         stage('Build') {
             steps {
-                sh 'docker run --rm -v ${PWD}:/code -w /code mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet build'
+                sh 'docker run --rm -v ${PWD}:/code -w /code -v ${PWD}/infrastructure/docker/dotnet/.nuget:/root/.nuget mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet build --no-restore'
             }
         }
         stage('Test') {
             steps {
-                sh 'docker run --rm -v ${PWD}:/code -w /code mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet test'
+                sh 'docker run --rm -v ${PWD}:/code -w /code -v ${PWD}/infrastructure/docker/dotnet/.nuget:/root/.nuget mcr.microsoft.com/dotnet/sdk:5.0-alpine dotnet test --no-build --verbosity normal'
             }
         }
     }
