9f2fd1bd6a629c2924c23319d9bfda59db7707ce
Alex Lanz <alex.lanz@aboutbits.it>
2021-08-25 14:58:03 +0200

    Specify AWS region

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -5,6 +5,7 @@ pipeline {
     environment {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+        AWS_DEFAULT_REGION = "eu-west-1"
 
         CLIENT_ID = credentials('noi-community-api-test-client-id')
         CLIENT_SECRET = credentials('noi-community-api-test-client-secret')
