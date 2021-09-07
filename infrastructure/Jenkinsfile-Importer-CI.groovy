17c7aa845d5ff3be6a8ade0d5285f628b2f4e0ec
Tobias Burger <tobias_burger@hotmail.com>
2021-09-07 16:04:53 +0200

    Add Jenkins infrastructure code

---
+++ infrastructure/Jenkinsfile-Importer-CI.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_IMAGE = "app"
+        DOCKER_TAG = "latest"
+        SERVER_PORT = "1234" 
+    }
+
+    stages {
+        stage('Configure') {
+            steps {
+                sh "rm -f .env"
+                sh "cp .env.example .env"
+                sh "echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env"
+                sh "echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env"
+                sh "echo 'SERVER_PORT=${SERVER_PORT}' >> .env"
+            }
+        }
+        stage('Test') {
+            steps {
+                sh "docker-compose -f O docker-compose.yml build"
+            }
+        }
+    }
+    post { 
+        always { 
+            sh 'docker-compose -f docker-compose.yml down || true'
+        }
+    }
+}
